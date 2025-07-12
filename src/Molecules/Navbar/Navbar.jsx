18425cba1945f49b85ccabca0f90299089575937
Leonardo <squirrelandrendy@gmail.com>
2025-07-12 15:02:52 -0400

    Fixing the title

--- src/Molecules/Navbar/Navbar.jsx
+++ src/Molecules/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import Title from "../../Atoms/Title/Title.jsx";
 const Navbar = () => {
   return (
     <div>
-      <Title text="CockyDeveloper" typeTitle="h1" />
+      <Title text="Hola amigos" typeTitle="h1" />
     </div>
   );
 };
